ea37f8707b6d8fe49cc6d4e1e1db13f2b1163523
imhamza1 <hamza.akram4635@gmail.com>
2021-04-15 22:17:52 +0500

    nav color modified

--- src/component/TopBar.jsx
+++ src/component/TopBar.jsx
@@ -1,37 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 // import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Button, FormControl, Toolbar, Typography } from '@material-ui/core';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Nav, Navbar} from 'react-bootstrap';
- const useStyles = makeStyles((theme) => ({
-    Name: {
-       color:"black",
-     },
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Button,
+  FormControl,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Nav, Navbar } from "react-bootstrap";
+const useStyles = makeStyles((theme) => ({
+  Name: {
+    backgroundColor: "black",
+    color: "white",
+  },
+}));
+const TopBar = () => {
+  const classes = useStyles();
+  return (
+    <Navbar className={classes.Name} variant="dark">
+      <Navbar.Brand href="/">
+        <em>Datalytics</em>
+      </Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/">Districts</Nav.Link>
+        <Nav.Link href="/">Schools</Nav.Link>
+        <Nav.Link href="/contact-us">Contact Us</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info" className={classes.Name}>
+          Search
+        </Button>
+      </Form>
+    </Navbar>
+  );
+};
 
-   }));
-const TopBar= () => {
-    
-    const classes = useStyles();
-     return ( 
-    
-    <Navbar bgcolor="" variant="light">
-    <Navbar.Brand  href="/"><em>Datalytics</em></Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/">Districts</Nav.Link>
-      <Nav.Link href="/">Schools</Nav.Link>
-      <Nav.Link href="/contact-us">Contact Us</Nav.Link>
-     
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info" className={classes.Name}>Search</Button>
-    </Form>
-  </Navbar>
-    );
-
-}
- 
 export default TopBar;
