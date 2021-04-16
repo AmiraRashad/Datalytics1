9f648fb7ab1a4d7a6f95d241d53d627451a86ba9
Amira Rashad <amira.rashad1996@hotmail.com>
2021-04-16 22:03:49 +0500

    color

--- src/component/TopBar.jsx
+++ src/component/TopBar.jsx
@@ -6,20 +6,22 @@ import { AppBar, Button, FormControl, Toolbar, Typography } from '@material-ui/c
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Nav, Navbar} from 'react-bootstrap';
  const useStyles = makeStyles((theme) => ({
-    Name: {
-       color:"black",
+    
+  Name: {
+       color: "#F39C12",
+       backgroundColor: " #084F86 ",
      },
-
+     
    }));
 const TopBar= () => {
     
     const classes = useStyles();
      return ( 
     
-    <Navbar bgcolor="" variant="light">
+    <Navbar className= "color-nav" bgcolor="" variant="dark">
     <Navbar.Brand  href="/"><em>Datalytics</em></Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link  href="/">Home</Nav.Link>
       <Nav.Link href="/">Districts</Nav.Link>
       <Nav.Link href="/">Schools</Nav.Link>
       <Nav.Link href="/contact-us">Contact Us</Nav.Link>
